b69822609760dcfdb33284fee0ef2ec038c63d97
aledenro <133998613+aledenro@users.noreply.github.com>
2024-03-24 20:07:06 -0600

    update a tablas sql

--- tablasProyecto.sql
+++ tablasProyecto.sql
@@ -13,19 +13,19 @@ flush privileges;
 use cookies_by_su;
 
 create table usuario(
+	id_usuario int auto_increment not null,
 	username varchar(50) not null unique,
-    cedula varchar(25) not null,
+    cedula varchar(25),
     nombre varchar(30) not null,
     apellidos varchar(50) not null,
-    pais varchar(25) not null,
-    provincia varchar(25) not null,
-    canton_distrito varchar(50) not null,
-    direccion_detallada varchar(200) not null,
+    pais varchar(25),
+    provincia varchar(25),
+    canton_distrito varchar(50),
+    direccion_detallada varchar(200),
     correo_electronico varchar(50) not null unique,
     contrasena varchar(25) not null,
-    rol varchar(25) not null,
-    numero_telefono varchar(20) not null,
-    primary key(username)
+    numero_telefono varchar(20),
+    primary key(id_usuario)
 )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
@@ -57,11 +57,11 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
 create table pedido(
-	num_pedido int not null auto_increment,
+	id_pedido int not null auto_increment,
     id_producto int not null,
     cantidad int not null,
     total double not null,
-    primary key(num_pedido),
+    primary key(id_pedido),
     foreign key(id_producto) references producto(id_producto)
 )
 ENGINE = InnoDB
@@ -69,8 +69,8 @@ DEFAULT CHARACTER SET = utf8mb4;
 
 create table facturacion(
 	id_facturacion int not null auto_increment,
-    username varchar(50) not null,
-    num_pedido int not null,
+    id_usuario int not null,
+    id_pedido int not null,
     direccion_detallada varchar(350) not null,
 	fecha_pedido date not null,
     fecha_entrega date not null,
@@ -81,23 +81,30 @@ create table facturacion(
     iva double,
     total_iva double,
     primary key(id_facturacion),
-    foreign key(username) references usuario(username),
-    foreign key(num_pedido) references pedido(num_pedido)
+    foreign key(id_usuario) references usuario(id_usuario),
+    foreign key(id_pedido) references pedido(id_pedido)
 )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
 create table form_contacto(
+	id_form int auto_increment not null,
 	nombre varchar(30) not null,
     apellidos varchar(50) not null,
     correo_electronico varchar(50) not null,
-    mensaje varchar(2000) not null
+    mensaje varchar(2000) not null,
+    primary key (id_form)
 )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
+create table roles(
+	id_rol int auto_increment not null,
+    nombre_rol varchar(25) not null,
+    id_usuario int,
+    primary key (id_rol),
+    foreign key (id_usuario) references usuario(id_usuario)
+);
+
 insert into producto(nombre_producto, descripcion, tipo, precio, ruta_imagen)
 select 'Galletas Nightmare Before Christmass', 'Tamaño: 10 cm <br> Set: 5 Galletas', 'Tamaño Completo', 12500.00, 'https://firebasestorage.googleapis.com/v0/b/cookies-by-su.appspot.com/o/cookies_by_Su%2Fproductos%2Fjack%20cookies.jpg?alt=media&token=69200b94-fdd4-4dfd-a805-0cb634963f19'; 
-
-use cookies_by_su;
-select p1_0.id_producto,p1_0.descripcion,p1_0.nombre_producto,p1_0.precio,p1_0.ruta_imagen,p1_0.tipo from producto p1_0 where p1_0.id_producto=1
